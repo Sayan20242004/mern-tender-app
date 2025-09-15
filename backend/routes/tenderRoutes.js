115c1295ece66f373e7fee970ceb109a5b604ed9
Sayan Kundu <your@email.com>
2025-09-15 22:56:33 +0530

    Clear all files from all-code branch

--- backend/routes/tenderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { fetchTenders, getTenderById } from "../controllers/tenderController.js";
-
-const router = express.Router();
-
-router.get("/", fetchTenders);
-router.get("/:id", getTenderById);
-
-export default router;
